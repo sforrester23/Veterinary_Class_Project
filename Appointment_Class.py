6d4e0b75e6b85bb34f11977cecca7e974d4a0a99
Sam Forrester <SForrester@spartaglobal.com>
2019-09-30 19:23:27 +0100

    Added class for Pet and appointment

---
+++ Appointment_Class.py
@@ -0,0 +1,8 @@
+# Define a class for Appointment
+class Appointment():
+    def __init__(self, ailment, date, pet, vet, cost):
+        self.ailment = ailment
+        self.date = date
+        self.pet = pet
+        self.vet = vet
+        self.cost = cost
---
+++ Pet_Class.py
@@ -0,0 +1,7 @@
+# Define a Pet class
+class Pet():
+    def __init__(self, name, owner, breed):
+        self.name = name
+        self.owner = owner
+        self.breed = breed
+        
